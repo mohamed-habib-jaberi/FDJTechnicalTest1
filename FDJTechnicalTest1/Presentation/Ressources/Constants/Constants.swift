ce4534f7720a027ea9fb5114e83382983e58b2e2
jaberi mohamed <mohamedhabibjaberi111@gmail.com>
2024-04-20 13:24:47 +0200

    add ressources

---
+++ FDJTechnicalTest1/Presentation/Ressources/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  FDJTechnicalTest1
+//
+//  Created by Jaberi  on 20/04/2024.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum Networking {
+        static let apiKeyNotValid = "Access Unauthorized"
+    }
+}
+
