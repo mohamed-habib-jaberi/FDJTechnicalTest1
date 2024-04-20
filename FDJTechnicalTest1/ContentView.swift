f65394d2baef7f4aa66aa98dd8bef4f5d921d882
jaberi mohamed <mohamedhabibjaberi111@gmail.com>
2024-04-20 13:06:23 +0200

    initial commit

---
+++ FDJTechnicalTest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDJTechnicalTest1
+//
+//  Created by Jaberi  on 20/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FDJTechnicalTest1/FDJTechnicalTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  FDJTechnicalTest1App.swift
+//  FDJTechnicalTest1
+//
+//  Created by Jaberi  on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FDJTechnicalTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
